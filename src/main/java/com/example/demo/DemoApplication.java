958617b677a5d54a5e6a0c10bca3d8c114cfe642
limingzhi <785423297@qq.com>
2019-04-21 18:43:42 +0800

    V0.6

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
 	
 	@RequestMapping
 	public String home(){
-		return "Hello Git v0.1!";
+		return "Hello Git v0.6!  --master update";
 	}
 
 	public static void main(String[] args) {
