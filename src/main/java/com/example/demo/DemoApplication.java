a0fc1278987cb7ed41a0b4ee4721f2581a53df88
limingzhi <785423297@qq.com>
2019-04-21 18:42:20 +0800

    V0.5

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
 	
 	@RequestMapping
 	public String home(){
-		return "Hello Git v0.1!";
+		return "Hello Git v0.1!  --branch king";
 	}
 
 	public static void main(String[] args) {
